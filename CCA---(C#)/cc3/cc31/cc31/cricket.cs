522bbf622b24d01a0f4e457438122e8296b21681
ayesham14 <ayesham@infinite.com>
2024-07-03 12:57:09 +0530

    a
    aa

---
+++ CCA---(C#)/cc3/cc31/cc31/cricket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc31
+{
+    public class cricket
+    {
+    public void Pointscalculation(int no_of_matches)
+        {
+            int[] scores = new int[no_of_matches];
+            int totalSum = 0;
+
+            for (int i = 0; i < no_of_matches; i++)   // each match input score
+            {
+                Console.Write($"Enter score for match {i + 1}: ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+                totalSum += scores[i];
+            }
+
+            double average = (double)totalSum / no_of_matches; // Calculating average
+
+           
+            Console.WriteLine($"Total Sum of Scores: {totalSum}");  //results
+            Console.WriteLine($"Average Score: {average}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            cricket c = new cricket(); // Creating an instance of Cricket class
+            Console.Write("Enter number of matches: "); // Input number of matches
+            int no_of_matches = Convert.ToInt32(Console.ReadLine());
+
+            // Call Pointscalculation method
+            c.Pointscalculation(no_of_matches);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CCA---(C#)/cc3/cc32/cc32/Box.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc32
+{
+    class Box
+    {
+        
+        public double Length { get; set; } // Properties land b
+        public double Breadth { get; set; }
+
+        public Box(double length, double breadth)
+        // Constructor
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        // Method to add two Box objects and store the result in a third Box object
+        public static Box AddBoxes(Box box1, Box box2)
+        {
+            double sumLength = box1.Length + box2.Length;
+            double sumBreadth = box1.Breadth + box2.Breadth;
+
+            return new Box(sumLength, sumBreadth);
+        }
+
+        // Method to display the details of a Box object
+        public void DisplayBoxDetails()
+        {
+            Console.WriteLine($"Length: {Length}");
+            Console.WriteLine($"Breadth: {Breadth}");
+            Console.WriteLine();
+        }
+    }
+
+    class Test
+    {
+        static void Main()
+        {
+            Box box1 = new Box(3.5, 2.5); // Creating the two Box objects
+            Box box2 = new Box(2.0, 3.5);
+
+            
+            Box box3 = Box.AddBoxes(box1, box2); // Adding the two Box objects and store the result in a third Box object
+
+
+            Console.WriteLine("Details of the Third Box:");  // Displaying details of the third Box object
+            box3.DisplayBoxDetails();
+
+            Console.ReadLine(); 
+
+        }
+
+    }
+}
